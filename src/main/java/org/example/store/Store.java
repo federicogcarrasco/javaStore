8bf00f8f33bb36cd8b1f16f199663ecc48fdd60e
Federico Carrasco <federicogcarrasco@gmail.com>
2023-08-27 17:19:14 -0300

    Refactor: Store stock to be a map of lists and add helper methods

--- src/main/java/org/example/store/Store.java
+++ src/main/java/org/example/store/Store.java
@@ -1,7 +1,10 @@
 package org.example.store;
 
 import org.example.product.Product;
-import java.util.HashMap;
+import org.example.product.packaged.PackagedProduct;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Store {
@@ -9,7 +12,11 @@ public class Store {
     private int maxAmountStockProducts;
     private int amountStockProducts = 0;
     private double cashBalance;
-    private final Map<String, Product> stock = new HashMap<>();
+    private final Map<Character, List<Product>> stock = Map.ofEntries(
+            Map.entry('B', new ArrayList<>()),
+            Map.entry('C', new ArrayList<>()),
+            Map.entry('Z', new ArrayList<>())
+    );
 
     public void buy(Product product) {
         try {
@@ -18,18 +25,31 @@ public class Store {
             if (amountStockProducts + product.getStock() > maxAmountStockProducts)
                 throw new Exception("The buy exceed the maximum amount of products allowed.");
 
-            Product productOldData = stock.get(product.getId());
+            Product productOldData = searchInStock(product.getId());
             if (productOldData != null)
                 product.setStock(product.getStock() + productOldData.getStock());
 
             product.setForSale(true);
-            stock.put(product.getId(), product);
+            getProductList(product.getId()).add(product);
             amountStockProducts += product.getStock();
             cashBalance -= product.getCost() * product.getStock();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private Product searchInStock(String id) {
+        List<Product> stockList = getProductList(id);
+        for (Product product : stockList) {
+            if (product.getId().equals(id))
+                return product;
+        }
+        return null;
+    }
+
+    private List<Product> getProductList(String id) {
+        return stock.get(id.charAt(1));
     }
 
     // other Getters and Setters...
